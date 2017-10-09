9805d32e84441d1ef37e6eed199b9754c498ed7d
XiaoLe <youngleocn@qq.com>
2017-10-09 21:28:42 +0800

    Add demo.sh

---
+++ bash/demo.sh
@@ -0,0 +1,13 @@
+if [ -d $1 ]; then
+  echo '目录已存在'
+  exit 1
+fi
+mkdir $1
+cd $1
+mkdir css js
+echo '<!DOCTYPE>' >> index.html
+echo '<title>Hello</title>' >> index.html
+echo '<h1>Hi</h1>' >> index.html
+echo 'h1{color: red;}' >> css/style.css
+echo 'var string = "Hello World"' >> js/main.js
+echo 'alert(string)' >> js/main.js
