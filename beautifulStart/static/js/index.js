52b571183e58c32e7841cfed67c69d8099d05602
XiaoLe <youngleocn@qq.com>
2017-11-10 16:29:12 +0800

    init beautifulStart

---
+++ beautifulStart/static/js/index.js
@@ -0,0 +1,14 @@
+let keyArray = [
+    ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
+    ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+    ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+    ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+]
+
+let defaultUrl = {
+    q:'qq.com',
+    w:'weibo.com',
+    e:'ele.me',
+    r:'renren.com'
+}
+
