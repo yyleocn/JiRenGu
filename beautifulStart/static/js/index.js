c20f4109ab4124e09125439acda540d746da1861
XiaoLe <youngleocn@qq.com>
2017-11-12 17:02:59 +0800

    beautifulStart 0.1 finish

--- beautifulStart/static/js/index.js
+++ beautifulStart/static/js/index.js
@@ -1,16 +1,6 @@
-let keyBoardArray = [
-    ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',],
-    ['tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',],
-    ['caps', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',],
-    ['shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm',],
-]
+'use strict';
 
-let defaultUrl = {
-    q: 'qq.com',
-    w: 'weibo.com',
-    e: 'ele.me',
-    r: 'renren.com',
-}
+/***** define function */
 
 let createTag = function (tag_, attr_) {
     let tagObj = document.createElement(tag_);
@@ -20,46 +10,132 @@ let createTag = function (tag_, attr_) {
         }
     }
     return tagObj;
-}
+};
 
-let getLocalStorage = function (key_, default_) {
-    return localStorage.getItem(key_) || default_;
-}
-
-
-let keyboardCreate = function (target_) {
-    if (!(target_ || {}).tagName) {
-        return;
+function configManage(name_, default_) {
+    'use strict';
+    if (!new.target) {
+        return new configManage(name_, default_);
     }
-    keyBoardArray.forEach(function (keyRow_, index_, arr_) {
+    let config = null;
+    try {
+        config = JSON.parse(localStorage.getItem(name_));
+    } catch (err_) {
+        console.log('Invalidation local storage config.');
+    }
+    if (!config) {
+        console.log('Load default config.');
+        config = default_;
+    }
+    this.getConfig = function (key_) {
+        return config[key_]
+    };
+    this.setConfig = function (key_, value_) {
+        config[key_] = value_;
+        this.saveConfig();
+    };
+    this.saveConfig = function () {
+        localStorage.setItem(name_, JSON.stringify(config))
+    }
+};
+
+let urlopen = function (keyboardUrlConfig_, event_) {
+    let key = event_.key
+    if (!keyboardUrlConfig_.getConfig(key)) {
+        return false;
+    }
+    window.open('http://' + keyboardUrlConfig_.getConfig(key), '_blank');
+}
+let keyboardCreate = function (box_, keyboardUrlConfig_, defaultFavicon_, blankFavicon_) {
+    if (!(box_ || {}).tagName) {
+        return false;
+    }
+    if (typeof (keyboardUrlConfig_) !== 'object' || !keyboardUrlConfig_.getConfig) {
+        return false;
+    }
+    keyboardLetterArray.forEach(function (keyRow_, index_, arr_) {
         let keyRowBox = createTag('div');
         keyRowBox.className = 'keyRow row' + index_;
         keyRow_.forEach(function (key_) {
             let keyObj = createTag(
-                'kbd',
-                {
+                'kbd', {
                     innerText: key_,
                     className: key_,
                 });
-            if (key_.length <= 1) {
-                keyObj.className = 'letter';
-            }
             let editButton = createTag(
-                'button',
-                {
+                'button', {
                     innerText: 'E',
                     type: 'button',
-                    className: 'editButton',
-                }
+                    className: 'editButton'
+                },
             );
+            if (key_.match(/^[a-z]$/i)) {
+                let favicon = {};
+                if (defaultFavicon_) {
+                    favicon = createTag(
+                        'img',
+                        {
+                            src: blankFavicon_,
+                            className: 'favicon'
+                        }
+                    );
+                    if (keyboardUrlConfig_.getConfig(key_)) {
+                        favicon.src = 'http://' + keyboardUrlConfig_.getConfig(key_) + '/favicon.ico'
+                    }
+                    favicon.onerror = function (event_) {
+                        if (event_.target.src !== defaultFavicon_) {
+                            event_.target.src = defaultFavicon_
+                        }
+                    }
+                    keyObj.appendChild(favicon);
+                }
+                keyObj.className = 'letter';
+                editButton.onclick = function () {
+                    let result = window.prompt('请输入' + key_ + '的快捷网址', '');
+                    if (result) {
+                        keyboardUrlConfig_.setConfig(key_, result);
+                        favicon.url = 'http://' + result + '/favicon.ico'
+                    }
+                }
+            }
             keyObj.appendChild(editButton);
             keyRowBox.appendChild(keyObj);
         });
-        target_.appendChild(keyRowBox);
-        target_.appendChild(document.createElement('br'));
+        box_.appendChild(keyRowBox);
+        box_.appendChild(document.createElement('br'));
     });
+    return true;
 }
 
-let keyboardBox = document.querySelector('#keyboard');
 
-keyboardCreate(keyboardBox);
+/***** define config */
+
+let keyboardLetterArray = [
+    ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0','-','=','back',],
+    ['tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p','[',']','\\',],
+    ['caps', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',';','\'','enter',],
+    ['shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm',',','.','/','shift',],
+];
+
+let keyboardUrlConfigDefault = {
+    q: 'qq.com',
+    w: 'weibo.com',
+    e: 'ele.me',
+    r: 'renren.com',
+};
+
+let configName = 'keyboardUrl';
+let defaultFavicon = './static/img/defaultFavicon.png'
+let blankFavicon = './static/img/blankFavicon.png'
+
+/***** process */
+let keyboardLinkConfig = configManage(configName, keyboardUrlConfigDefault);
+let keyboardBox = document.querySelector('#keyboard', {});
+let initRes = keyboardCreate(
+    keyboardBox, keyboardLinkConfig,
+    defaultFavicon, blankFavicon
+);
+document.onkeypress = urlopen.bind(null, keyboardLinkConfig);
+if (!initRes) {
+    console.log('Init fail.')
+}
