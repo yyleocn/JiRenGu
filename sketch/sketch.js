f5341597f6bd940b22c28e8075c551b0c6f74661
XiaoLe <youngleocn@qq.com>
2017-11-21 15:22:10 +0800

    finish touch mouse event

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -11,6 +11,43 @@ let createTag = function (tag_, attr_) {
     return tagObj;
 };
 
+
+
+let canvasFillCircle = function (context_, x_, y_, radius_, color_) {
+    if (!context_ instanceof CanvasRenderingContext2D) {
+        throw 'Invalid canvas object!';
+    }
+    context_.beginPath();
+    context_.arc(
+        x_, y_, radius_,
+        0, Math.PI * 2,
+    );
+    context_.fillStyle = color_;
+    context_.fill();
+    context_.closePath();
+};
+
+let canvasFillLine = function (context_, fromX_, fromY_, toX_, toY_, width_, color_) {
+    if (!context_ instanceof CanvasRenderingContext2D) {
+        throw 'Invalid canvas object!';
+    }
+    context_.beginPath();
+    context_.moveTo(fromX_, fromY_);
+    context_.lineTo(toX_, toY_);
+    context_.lineWidth = width_;
+    context_.strokeStyle = color_;
+    context_.stroke();
+    context_.closePath();
+};
+
+let isTouchDevice = function () {
+    return 'ontouchstart' in window;
+}
+
+let getEvent = function (event_) {
+    console.log(event_);
+};
+
 let sketch = function (target_) {
     'use strict';
     if (!(this instanceof sketch)) {
@@ -18,13 +55,40 @@ let sketch = function (target_) {
         return new sketch(target_);
     };
 
+
     //***** init config */
     let config = {
-        width: 5,
+        radius: 2,
         mode: 'default',
         color: '#000',
+        mouseClicking: false,
+        lastPointX: 0,
+        lastPointY: 0,
+
     };
 
+    //***** get event offset */
+    let getEventOffset = function (event_, target_) {
+        if (event_ instanceof TouchEvent) {
+            let targetTouche = event_.targetTouches[0];
+            let targetOffsetX = target_.offsetLeft;
+            let targetOffsetY = target_.offsetTop;
+            return {
+                x: targetTouche.clientX - targetOffsetX,
+                y: targetTouche.clientY - targetOffsetY,
+            };
+        }
+        if (event_ instanceof Event) {
+            return {
+                x: event_.offsetX,
+                y: event_.offsetY,
+            };
+        }
+        return {
+            x: 0,
+            y: 0,
+        }
+    };
     //** init toolBar */
     let toolBar = createTag('div', {
         className: 'toolBar',
@@ -35,6 +99,7 @@ let sketch = function (target_) {
     let canvasObj = createTag('canvas', {
         innerHtml: '浏览器不支持Canvas',
     });
+    let canvasContext = canvasObj.getContext('2d');
     let windowResize = function () {
         canvasObj.height = target_.clientHeight - toolBar.clientHeight;
         canvasObj.width = target_.clientWidth;
@@ -42,6 +107,22 @@ let sketch = function (target_) {
     windowResize();
     window.addEventListener('resize', windowResize);
     target_.appendChild(canvasObj);
+
+    //***** canvas event bind */
+    let canvasEventBind = function (target_, start_, move_, end_) {};
+    if (isTouchDevice()) {
+        canvasEventBind = function (target_, start_, move_, end_) {
+            target_.ontouchstart = start_;
+            target_.ontouchmove = move_;
+            target_.ontouchend = end_;
+        };
+    } else {
+        canvasEventBind = function (target_, start_, move_, end_) {
+            target_.onmousedown = start_;
+            target_.onmousemove = move_;
+            target_.onmouseup = end_;
+        };
+    }
 
     //** init colorPicker */
     let colorPicker = createTag('input', {
@@ -63,18 +144,75 @@ let sketch = function (target_) {
         type: 'range',
         className: 'widthPicker',
         min: 1,
-        max: 10,
-        value: config.width,
+        max: 5,
+        value: config.radius,
     });
     let widthPickerChange =
         function (event_) {
-            config.width = parseInt(event_.target.value);
+            config.radius = parseInt(event_.target.value);
         }
     widthPicker.addEventListener(
         'change',
         widthPickerChange
     );
     toolBar.appendChild(widthPicker);
+
+
+
+    /***** define click event */
+    let saveBtn = createTag('i', {
+        className: ['iconfont', 'icon-download', ],
+        mode: 'default',
+    });
+    saveBtn.addEventListener('click', function (event_) {
+        console.log(event_);
+    })
+    toolBar.appendChild(saveBtn);
+
+    /***** event bind cache list */
+    let clearEventBind = function () {};
+
+    /***** define click event bind */
+    let modeEventBind = {
+        'pen': {
+            'mouseDown': function (event_) {
+                let eventOffset = getEventOffset(event_, canvasObj);
+                canvasFillCircle(
+                    canvasContext,
+                    eventOffset.x, eventOffset.y,
+                    config.radius, config.color,
+                )
+                //***** update config */
+                config.lastPointX = eventOffset.x;
+                config.lastPointY = eventOffset.y;
+                config.mouseClicking = true;
+            },
+            'mouseMove': function (event_) {
+                if (!config.mouseClicking) {
+                    return undefined;
+                }
+                let eventOffset = getEventOffset(event_, canvasObj);
+                console.log(eventOffset);
+                canvasFillLine(
+                    canvasContext,
+                    config.lastPointX, config.lastPointY,
+                    eventOffset.x, eventOffset.y,
+                    config.radius * 2, config.color,
+                );
+                canvasFillCircle(
+                    canvasContext,
+                    eventOffset.x, eventOffset.y,
+                    config.radius, config.color,
+                );
+                //***** update config */
+                config.lastPointX = eventOffset.x;
+                config.lastPointY = eventOffset.y;
+            },
+            'mouseUp': function (event_) {
+                config.mouseClicking = false;
+            },
+        },
+    };
 
     //** init modePicker */
     let modePick = function (event_) {
@@ -83,8 +221,35 @@ let sketch = function (target_) {
             return;
         }
         config.mode = target.mode;
-        target.parentNode.querySelector('.modePicker.active').classList.remove('active');
+        if (target.parentNode.querySelector('.modePicker.active')) {
+            target.parentNode.querySelector('.modePicker.active').classList.remove('active');
+        }
         target.classList.add('active');
+        switch (target.mode) {
+            case 'pen':
+                {
+                    canvasEventBind(
+                        canvasObj,
+                        modeEventBind.pen.mouseDown,
+                        modeEventBind.pen.mouseMove,
+                        modeEventBind.pen.mouseUp
+                    );
+                    break;
+                }
+            case 'eraser':
+                {
+                    break;
+                }
+            default:
+                {
+                    canvasEventBind(
+                        canvasObj,
+                        null,
+                        null,
+                        null,
+                    );
+                }
+        }
     };
 
 
@@ -101,59 +266,22 @@ let sketch = function (target_) {
     });
     modePen.onclick = modePick;
     toolBar.appendChild(modePen);
+    modePen.click();
 
     let modeDefault = createTag('i', {
-        className: ['iconfont', 'icon-hand', 'modePicker', 'active', ],
+        className: ['iconfont', 'icon-hand', 'modePicker', ],
         mode: 'default',
     });
     modeDefault.onclick = modePick;
     toolBar.appendChild(modeDefault);
 
-    /***** define click event */
-    let saveBtn = createTag('i', {
-        className: ['iconfont', 'icon-download', ],
-        mode: 'default',
-    });
-    toolBar.appendChild(saveBtn);
-
-    /***** define click event */
-    let mouseClick = function (event_) {
-        switch (config.mode) {
-            case 'pen':
-                {
-                    break;
-                }
-            case 'eraser':
-                {
-                    break;
-                }
-            default:
-                {
-
-                }
-        }
-    }
-
-    let mouseMove = function (event_) {
-        switch (config.mode) {
-            case 'pen':
-                {
-                    break;
-                }
-            case 'eraser':
-                {
-                    break;
-                }
-            default:
-                {
-
-                }
-        }
-    }
-
-    let mouseRelease = function (event_) {
-
-    }
+    /***** debug */
+    console.log([
+        target_,
+        config,
+        canvasObj,
+        canvasContext,
+    ])
 }
 
 let sketchObj = document.querySelector('#sketch');
