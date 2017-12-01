49baaf666053c6ceebefdd355a86a550141679bd
XiaoLe <youngleocn@qq.com>
2017-12-01 20:48:06 +0800

    finish save function

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -247,25 +247,12 @@ function Sketch(target_) {
         // anchorDom.click();
 
 
-
         /** new tab method */
         let imgData = canvasObj.toDataURL("image/png");
         let newTab = window.open('', '_blank');
         newTab.document.write(
-            '<iframe style="height:90vh;width:90vw" src="' + imgData + '"></iframe>'
-        )
-        // let imgDom = newTab.document.createElement('img');
-        // imgDom.src = imgData;
-        // let anchorDom = newTab.document.createElement('a');
-        // anchorDom.href = imgData;
-        // anchorDom.target_ = '_blank';
-        // anchorDom.download = 'sketch.png';
-        // anchorDom.innerHTML = '下载';
-        // newTab.document.body.appendChild(imgDom);
-        // newTab.document.body.appendChild(
-        //     newTab.document.createElement('br')
-        // );
-        // newTab.document.body.appendChild(anchorDom);
+            '<img src="' + imgData + '"></iframe>'
+        );
     }
     saveButton.addEventListener('click', canvasSave);
 
