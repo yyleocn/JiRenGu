e221ef3997c892f67efe33d60fb141da9a8a25b7
XiaoLe <youngleocn@qq.com>
2018-01-31 11:11:14 +0800

    finish resume-dynamic

--- resume-dynamic/js/main.js
+++ resume-dynamic/js/main.js
@@ -1,36 +1,112 @@
 !function () {
-    let mainCSS = document.querySelector('#cssBox');
-    let mainCSSText = document.querySelector('#mainCSSText');
-    let mainCSSRender = (content_) => {
-        mainCSS.innerHTML = content_;
-        mainCSSText.innerHTML = Prism.highlight(content_, Prism.languages.css);
-        mainCSSText.scrollTop = 9999;
+    let DEBUG = false;
+    let cssDom = document.querySelector('#main-css');
+    let editorDom = document.querySelector('#editor');
+    let paperDom = document.querySelector('#paper');
+
+    let editorRender = (content_) => {
+        cssDom.innerHTML = content_;
+        editorDom.innerHTML = Prism.highlight(content_, Prism.languages.css);
+        editorDom.scrollTop = 9999;
     };
 
-    let mainCSSAutoRender = (content_, callback_) => {
+    let paperRender = (content_) => {
+        paperDom.innerHTML = content_;
+    };
+
+    let autoRender = ({content_, render_, callback_, startContent_,}) => {
         let i = 0;
-        let baseText = mainCSS.innerHTML;
-        intervalHandler = setInterval(() => {
+        let baseContent = startContent_ || '';
+        if (DEBUG) {
+            render_(baseContent + content_);
+            if (callback_) {
+                callback_();
+            }
+            return;
+        }
+        let intervalHandler = setInterval(() => {
             i++;
             if (i >= content_.length) {
                 clearInterval(intervalHandler);
-                intervalHandler = undefined;
                 if (callback_) {
                     callback_();
                 }
                 return;
             }
-            mainCSSRender(baseText + content_.substr(0, i));
+            render_(baseContent + content_.substr(0, i));
         }, 30);
     };
 
-    let intervalHandler = undefined;
+
     new Promise((resolve_, reject_) => {
         $.ajax({
-            url: './css/main.css',
+            url: './css/code-01.css',
             success: (data_) => {
-                mainCSSAutoRender(data_ + '\n', resolve_);
+                autoRender({
+                    render_: editorRender,
+                    startContent_: cssDom.innerHTML,
+                    content_: data_ + '\n',
+                    callback_: resolve_,
+                });
             }
+        });
+    }).then(() => {
+        return new Promise((resolve_, reject_) => {
+            $.ajax({
+                url: './static/self-introduction.md',
+                success: (data_) => {
+                    autoRender({
+                        render_: paperRender,
+                        startContent_: paperDom.innerHTML,
+                        content_: data_ + '\n',
+                        callback_: resolve_,
+                    });
+                }
+            });
+        });
+    }).then(() => {
+        return new Promise((resolve_, reject_) => {
+            $.ajax({
+                url: './css/code-02.css',
+                success: (data_) => {
+                    autoRender({
+                        render_: editorRender,
+                        startContent_: cssDom.innerHTML,
+                        content_: data_ + '\n',
+                        callback_: resolve_,
+                    });
+                }
+            });
+        });
+    }).then(() => {
+        return new Promise((resolve_, reject_) => {
+            marked.setOptions({
+                renderer: new marked.Renderer(),
+                gfm: true,
+                tables: true,
+                breaks: false,
+                pedantic: false,
+                sanitize: false,
+                smartLists: true,
+                smartypants: false,
+                xhtml: false
+            });
+            paperDom.innerHTML = marked(paperDom.innerHTML).replace(/\r\n|\r|\n/g, '');
+            resolve_();
+        })
+    }).then(() => {
+        return new Promise((resolve_, reject_) => {
+            $.ajax({
+                url: './css/code-03.css',
+                success: (data_) => {
+                    autoRender({
+                        render_: editorRender,
+                        startContent_: cssDom.innerHTML,
+                        content_: data_ + '\n',
+                        callback_: resolve_,
+                    });
+                }
+            });
         });
     });
 }();
---
+++ resume-dynamic/static/self-introduction.md
@@ -0,0 +1,26 @@
+# 自我介绍
+
+我是小乐，1989 年出生  
+自学前端半年
+
+# 技能介绍
+
+熟悉 JavaScript CSS Python
+
+# 项目介绍
+
+* 轮播
+* 简历
+* 画板
+
+# 爱好
+
+* 游戏
+* 篮球
+* 美食
+
+# 联系方式
+
+* QQ 99589xxxxx
+* Email youngleocn(at)qq.com
+* 手机 186xxxxxxxx
